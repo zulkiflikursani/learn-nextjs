227a3b8e570da7637fe4c8a47a90d18872a9a981
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-03-17 01:07:26 +0800

    comlited

---
+++ app/dashboard/(overview)/invoices/[id]/edit/loading.tsx
@@ -0,0 +1,13 @@
+import InvoicesTable from '@/app/ui/invoices/table';
+import DashboardSkeleton, {
+  InvoiceSkeleton,
+  InvoicesTableSkeleton,
+} from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return (
+    <>
+      <InvoiceSkeleton />
+    </>
+  );
+}
---
+++ app/lib/actions.tsx
@@ -0,0 +1,128 @@
+'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import { signIn } from '../../auth';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater thank $0' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
+  date: z.string(),
+});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+async function action(fromData: FormData) {
+  return <div></div>;
+}
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  // Test it out:
+  // console.log(' amount ', rawFormData.amount);
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.error('Database Error:', error);
+    // throw new Error('Failed to create invoice.');
+    return {
+      message: 'Database error : failed to Create invoices',
+    };
+  }
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+  } catch (error) {
+    return {
+      message: 'Database erro : failed to update invoices',
+    };
+  }
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error('Failed to delete data');
+  try {
+    await sql`DELETE from invoices WHERE id = ${id} `;
+    console.log('deleted data');
+    revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    return {
+      message: 'Deleted Error ' + error,
+    };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    console.log('start login');
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+export default action;
